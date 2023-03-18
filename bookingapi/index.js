8fddce95e6d059fe273e606836049a5e5f94c4aa
Vikram Kumar Meena <vikramkr.meena.min16@itbhu.ac.in>
2023-03-18 23:31:32 +0530

    changes

--- bookingapi/index.js
+++ bookingapi/index.js
@@ -9,6 +9,7 @@ import cookieParser from 'cookie-parser'
 import cors from "cors"
 const app = express();
 dotenv.config()
+const PORT = process.env.PORT || 8800
 
 mongoose.set("strictQuery", false);
 
@@ -49,7 +50,7 @@ app.use((err, req, res, next) => {
 
 })
 
-app.listen(8800, () => {
+app.listen(PORT, () => {
     connect();
     console.log("Connected to backend.");
 })
